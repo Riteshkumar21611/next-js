1cf209941b563c466e877de34da72a37a8b41ce3
riteshCrimsonbeans <riteshverma2161@gmail.com>
2025-01-27 12:18:16 +0530

    some new update

--- src/app/Home/page.jsx
+++ src/app/Home/page.jsx
@@ -4,7 +4,7 @@ export default function Home() {
   
   return (
     <div>
-      <h1>Welcome to Your First Server Component!</h1>
+      <h1>Welcome to Your First !</h1>
       <ServerComponent />
     </div>
   );
